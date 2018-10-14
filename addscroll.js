a6c11a67b7b2809b8714c095f44de0eff57ee1d5
Harshit Singh <33094301+theharshguy99@users.noreply.github.com>
2018-10-14 15:14:36 +0530

    Added initial files

---
+++ addscroll.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+	var elem = document.createElement('div');
+	elem.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000';
+	document.body.appendChild(elem);
+}
